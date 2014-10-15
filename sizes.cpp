20fafa1ced602681a388a36c04de1a08f1028aca
Paul J. Lucas <paul@lucasmail.org>
2014-10-15 14:22:48 -0700

    Reduced specialization code; added comments.

--- sizes.cpp
+++ sizes.cpp
@@ -1,3 +1,24 @@
+/*
+**      sizes -- print C++ types' sizes and limits
+**      sizes.cpp
+**
+**      Copyright (C) 2014  Paul J. Lucas
+**
+**      This program is free software; you can redistribute it and/or modify
+**      it under the terms of the GNU General Public License as published by
+**      the Free Software Foundation; either version 2 of the Licence, or
+**      (at your option) any later version.
+** 
+**      This program is distributed in the hope that it will be useful,
+**      but WITHOUT ANY WARRANTY; without even the implied warranty of
+**      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**      GNU General Public License for more details.
+** 
+**      You should have received a copy of the GNU General Public License
+**      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* system */
 #include <iomanip>
 #include <iostream>
 #include <limits>
@@ -9,39 +30,40 @@ int const name_width   = 13;
 int const sizeof_width =  2;
 
 template<typename T>
-void print_type_info( char const *type_name, T ) {
-  cout << left << setw( name_width ) << type_name << right
-       << setw( sizeof_width ) << sizeof( T )
-       << setw( limit_width ) << numeric_limits<T>::min()
-       << setw( limit_width ) << numeric_limits<T>::max()
-       << endl;
+inline T number_limit( T t ) {
+  return t;
 }
 
 // Specialization for char so the limits print as int rather than char.
-void print_type_info( char const *type_name, char ) {
+inline int number_limit( char c ) {
+  return static_cast<int>( c );
+}
+
+template<typename T>
+void print_type_info( char const *type_name, T ) {
   cout << left << setw( name_width ) << type_name << right
-       << setw( sizeof_width ) << sizeof( char )
-       << setw( limit_width ) << (int)numeric_limits<char>::min()
-       << setw( limit_width ) << (int)numeric_limits<char>::max()
+       << setw( sizeof_width ) << sizeof( T )
+       << setw( limit_width ) << number_limit( numeric_limits<T>::min() )
+       << setw( limit_width ) << number_limit( numeric_limits<T>::max() )
        << endl;
 }
 
-#define PRINT(T) print_type_info( #T, static_cast<T>(0) )
+#define PRINT_TYPE_INFO(T) print_type_info( #T, static_cast<T>(0) )
 
 int main() {
-  PRINT( bool );
-  PRINT( char );
-  PRINT( wchar_t );
-  PRINT( short );
-  PRINT( int );
-  PRINT( long );
-  PRINT( long long );
-  PRINT( float );
-  PRINT( double );
-  PRINT( long double );
-  PRINT( void* );
-  PRINT( size_t );
-  PRINT( ptrdiff_t );
+  PRINT_TYPE_INFO( bool );
+  PRINT_TYPE_INFO( char );
+  PRINT_TYPE_INFO( wchar_t );
+  PRINT_TYPE_INFO( short );
+  PRINT_TYPE_INFO( int );
+  PRINT_TYPE_INFO( long );
+  PRINT_TYPE_INFO( long long );
+  PRINT_TYPE_INFO( float );
+  PRINT_TYPE_INFO( double );
+  PRINT_TYPE_INFO( long double );
+  PRINT_TYPE_INFO( void* );
+  PRINT_TYPE_INFO( size_t );
+  PRINT_TYPE_INFO( ptrdiff_t );
   return 0;
 }
 
