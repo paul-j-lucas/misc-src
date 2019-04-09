5b42d92bbbb96983b0cad71a1fd9fb40e303ee47
Paul J. Lucas <plucas@splunk.com>
2019-04-09 10:43:38 -0700

    More comments.

--- c_private.h
+++ c_private.h
@@ -5,7 +5,7 @@
 
 /**
  * @file
- * These macros can be used to have "private" (sort of) struct members in C.
+ * These macros can be used to have "private" (sort of) `struct` members in C.
  * For example:
  *
  *    struct my_struct {
@@ -22,10 +22,26 @@
  *    struct my_struct s;
  *    // ...
  *    C_PRIVATE(s).shhh = 0;
+ *
+ * @note There can be at most one "private" section per `struct`.
  */
 
+/**
+ * Begins a "private" section of a `struct`.
+ * @note There can be at most one "private" section per `struct`.
+ */
 #define C_PRIVATE_BEGIN struct {
+
+/**
+ * Ends a "private" section of a `struct`.
+ */
 #define C_PRIVATE_END   } c_private;
+
+/**
+ * Accesses a "private" member of a `struct.
+ *
+ * @param OBJ The `struct` object to access a "private" member of.
+ */
 #define C_PRIVATE(OBJ)  ((OBJ).c_private)
 
 ///////////////////////////////////////////////////////////////////////////////
